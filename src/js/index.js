1f09e585b02e362b6802e0efc2be82157a7948a1
RinkuMonani <monanira@gmail.com>
2020-07-07 01:14:44 +0530

    Added event listener for 'submit' event on Search button - defined strcuture for controlSearch() to return recipe upon hiting search button

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,40 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResult(query){
-    try{
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`); //returns in json
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    }
-    catch(error){
-        alert(error);
-    }
+/* 
+GLOBAL STATE OF THE APP
+- Search object
+- Current recipe Object
+- shopping list object
+- liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1. get query from view
+    const query = 'pizza';
     
+    if(query){
+        //2. create new search object
+        //   add it to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.recipes);
+
+    }
+    // else{
+
+    // }
 }
 
-getResult("onion");
+document.querySelector('.search').addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+});
 
-// https://forkify-api.herokuapp.com/api/search
+// search.getResult();
